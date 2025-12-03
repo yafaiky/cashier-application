fe2556b97fb84b00bfebeabe683533fe4c1e4e2c
yafaiky <yafaiky@gmail.com>
2025-12-04 00:12:15 +0700

    kita menambahkan CRUD untuk produk

--- app/Livewire/Produk.php
+++ app/Livewire/Produk.php
@@ -2,12 +2,99 @@
 
 namespace App\Livewire;
 
+use App\Models\Produk as ModelProduk;
 use Livewire\Component;
 
 class Produk extends Component
 {
+    public $pilihanMenu = 'lihat';
+    public $nama;
+    public $kode;
+    public $harga;
+    public $stok;
+    public $produkTerpilih;
+
+    public function pilihMenu($menu)
+    {
+        $this->resetExcept('pilihanMenu');
+        $this->pilihanMenu = $menu;
+    }
+
+    public function pilihEdit($id)
+    {
+        $this->produkTerpilih = ModelProduk::findOrFail($id);
+
+        $this->nama  = $this->produkTerpilih->nama;
+        $this->kode  = $this->produkTerpilih->kode;
+        $this->harga = $this->produkTerpilih->harga;
+        $this->stok  = $this->produkTerpilih->stok;
+
+        $this->pilihanMenu = 'edit';
+    }
+
+    public function simpanEdit()
+    {
+        $this->validate([
+            'nama' => 'required',
+            'kode' => 'required|unique:produks,kode,' . $this->produkTerpilih->id,
+            'harga' => 'required',
+            'stok' => 'required',
+        ]);
+
+        $produk = $this->produkTerpilih;
+        $produk->nama = $this->nama;
+        $produk->kode = $this->kode;
+        $produk->harga = $this->harga;
+        $produk->stok = $this->stok;
+        $produk->save();
+
+        $this->reset();
+        $this->pilihanMenu = 'lihat';
+    }
+
+    public function pilihHapus($id)
+    {
+        $this->produkTerpilih = ModelProduk::findOrFail($id);
+        $this->pilihanMenu = 'hapus';
+    }
+
+    public function hapus()
+    {
+        $this->produkTerpilih->delete();
+        $this->reset();
+        $this->pilihanMenu = 'lihat';
+    }
+
+    public function batal()
+    {
+        $this->reset();
+        $this->pilihanMenu = 'lihat';
+    }
+
+    public function simpan()
+    {
+        $this->validate([
+            'nama' => 'required',
+            'kode' => 'required|unique:produks,kode',
+            'harga' => 'required',
+            'stok' => 'required',
+        ]);
+
+        $produk = new ModelProduk();
+        $produk->nama = $this->nama;
+        $produk->kode = $this->kode;
+        $produk->harga = $this->harga;
+        $produk->stok  = $this->stok;
+        $produk->save();
+
+        $this->reset();
+        $this->pilihanMenu = 'lihat';
+    }
+
     public function render()
     {
-        return view('livewire.produk');
+        return view('livewire.produk', [
+            'semuaProduk' => ModelProduk::all()
+        ]);
     }
 }
--- resources/views/livewire/produk.blade.php
+++ resources/views/livewire/produk.blade.php
@@ -1,3 +1,152 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <div class="container">
+
+        <div class="row my-2">
+            <div class="col-12">
+                <button wire:click="pilihMenu('lihat')"
+                    class="btn {{ $pilihanMenu == 'lihat' ? 'btn-primary' : 'btn-outline-primary' }}">
+                    Semua Produk
+                </button>
+
+                <button wire:click="pilihMenu('tambah')"
+                    class="btn {{ $pilihanMenu == 'tambah' ? 'btn-primary' : 'btn-outline-primary' }}">
+                    Tambah Produk
+                </button>
+
+                <button wire:loading class="btn btn-info">
+                    Loading
+                </button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+
+                @if ($pilihanMenu == 'lihat')
+                    <div class="card border-primary">
+                        <div class="card-header">Semua Produk</div>
+
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Kode</th>
+                                        <th>Nama</th>
+                                        <th>Harga</th>
+                                        <th>Stok</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    @foreach ($semuaProduk as $produk)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $produk->kode }}</td>
+                                            <td>{{ $produk->nama }}</td>
+                                            <td>{{ $produk->harga }}</td>
+                                            <td>{{ $produk->stok }}</td>
+                                            <td>
+                                                <button wire:click="pilihEdit({{ $produk->id }})"
+                                                    class="btn btn-outline-primary">
+                                                    Edit
+                                                </button>
+
+                                                <button wire:click="pilihHapus({{ $produk->id }})"
+                                                    class="btn btn-outline-danger">
+                                                    Hapus
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+
+                            </table>
+                        </div>
+                    </div>
+
+                @elseif ($pilihanMenu == 'tambah')
+                    <div class="card border-primary">
+                        <div class="card-header">Tambah Produk</div>
+
+                        <div class="card-body">
+                            <form wire:submit.prevent='simpan'>
+                                <label>Nama</label>
+                                <input type="text" class="form-control" wire:model='nama'>
+                                @error('nama') <span class="text-danger">{{ $message }}</span> @enderror
+                                <br>
+
+                                <label>Kode</label>
+                                <input type="text" class="form-control" wire:model='kode'>
+                                @error('kode') <span class="text-danger">{{ $message }}</span> @enderror
+                                <br>
+
+                                <label>Harga</label>
+                                <input type="number" class="form-control" wire:model='harga'>
+                                @error('harga') <span class="text-danger">{{ $message }}</span> @enderror
+                                <br>
+
+                                <label>Stok</label>
+                                <input type="number" class="form-control" wire:model='stok'>
+                                @error('stok') <span class="text-danger">{{ $message }}</span> @enderror
+                                <br>
+
+                                <button type="submit" class="btn btn-primary mt-3">SIMPAN</button>
+                            </form>
+                        </div>
+                    </div>
+
+                @elseif ($pilihanMenu == 'edit')
+                    <div class="card border-primary">
+                        <div class="card-header">Edit Produk</div>
+
+                        <div class="card-body">
+                            <form wire:submit.prevent='simpanEdit'>
+                                <label>Nama</label>
+                                <input type="text" class="form-control" wire:model='nama'>
+                                @error('nama') <span class="text-danger">{{ $message }}</span> @enderror
+                                <br>
+
+                                <label>Kode</label>
+                                <input type="text" class="form-control" wire:model='kode'>
+                                @error('kode') <span class="text-danger">{{ $message }}</span> @enderror
+                                <br>
+
+                                <label>Harga</label>
+                                <input type="number" class="form-control" wire:model='harga'>
+                                @error('harga') <span class="text-danger">{{ $message }}</span> @enderror
+                                <br>
+
+                                <label>Stok</label>
+                                <input type="number" class="form-control" wire:model='stok'>
+                                @error('stok') <span class="text-danger">{{ $message }}</span> @enderror
+                                <br>
+
+                                <button type="submit" class="btn btn-primary mt-3">SIMPAN</button>
+                            </form>
+                        </div>
+                    </div>
+
+                @elseif ($pilihanMenu == 'hapus')
+                    <div class="card border-danger">
+                        <div class="card-header bg-danger text-white">
+                            Hapus Produk
+                        </div>
+
+                        <div class="card-body">
+                            Apakah kamu yakin akan menghapus produk ini?
+                            <p>Kode : {{ $produkTerpilih->kode }}</p>
+                            <p>Nama : {{ $produkTerpilih->nama }}</p>
+
+                            <button class="btn btn-danger" wire:click='hapus'>HAPUS</button>
+                            <button class="btn btn-secondary" wire:click='batal'>BATAL</button>
+                        </div>
+                    </div>
+                @endif
+
+            </div>
+        </div>
+
+    </div>
 </div>
